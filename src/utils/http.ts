2f2fb8ec2fbe0dcb39c7f0268f3f00e7d4494f43
zhangjing <872958976@qq.com>
2021-06-01 14:57:28 +0800

    TS的Utility Types-Pick、Exclude、Partial和Omit实现

--- src/utils/http.ts
+++ src/utils/http.ts
@@ -45,7 +45,39 @@ export const http = async (endpoint: string, { data,token,headers,...customConfi
 // 5-8 用useHttp管理JWT和登录状态，保持登录状态
 export const useHttp=()=>{
     const {user}=useAuth()
-    //TODO 讲解 TS 操作符
+    //TODO 讲解 TS 操作符 Utility Types
+    //JS中的typeof是在runtime时运行的，TS的是在静态环境中运行的
     return (...[endpoint,config]:Parameters<typeof http>)=>http(endpoint,{...config,token:user?.token})
     // return ([endpoint,config]:[string,Config])=>http(endpoint,{...config,token:user?.token})
 }
+
+//联合类型
+let myNumber:string|number|{}
+myNumber=7
+
+//类型别名在很多情况下可以和interface互换
+// interface Person{
+//     name:string
+// }
+// type Person={name?:string}
+// const lili:Person={name:"lili"}
+
+//类型别名 在这种情况下不可以和interface互换
+type FavourtiteNumber=string|number
+let hisFvoriteNumber:FavourtiteNumber=6
+
+//interface也无法实现Utility type
+type Person={
+    name:string,
+    age:number
+}
+const xiaoli:Partial<Person>={name:'xiaoli'}
+const lilei:Omit<Person,'name'|'age'>={age:8}
+type PersonKeys=keyof Person
+type PersonOnlyName=Pick<Person,'name'|'age'>
+type Age=Exclude<PersonKeys,'name'>
+
+//Partial的实现
+// type Partial<T> = {
+//     [P in keyof T]?: T[P];
+// };
